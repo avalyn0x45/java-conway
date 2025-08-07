f020bbac4645b7485eb1a5bc4552fe61edd66a46
Avalyn Baldyga <jackedak@gmail.com>
2025-08-07 18:40:14 -0400

    License (pub domain bc idgaf about this code)

--- src/main/java/com/avalyn/CGOL/App.java
+++ src/main/java/com/avalyn/CGOL/App.java
@@ -1,3 +1,5 @@
+// This code is in the public domain. Original author Avalyn Baldyga
+
 package com.avalyn.CGOL;
 import java.util.*;
 import java.nio.charset.Charset;
--- src/main/java/com/avalyn/CGOL/ControlCodes.java
+++ src/main/java/com/avalyn/CGOL/ControlCodes.java
@@ -1,3 +1,5 @@
+// This code is in the public domain. Original author Avalyn Baldyga
+
 package com.avalyn.CGOL;
 
 public class ControlCodes {
